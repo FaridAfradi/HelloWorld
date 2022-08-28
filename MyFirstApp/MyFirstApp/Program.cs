612e918a8a078c5b10c869e3bf72a1efbcf56b57
FaridAfradi <Farid@afradi.se>
2022-08-28 15:31:11 +0200

    Inlämning

---
+++ MyFirstApp/MyFirstApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstApp {
+    internal class Program {
+        static void Main(string[] args) {
+
+            Console.WriteLine("Hello! My name is Farid");
+        }
+    }
+}
